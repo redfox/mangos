34ce24ed51a62981330426aa24fa5529d301309f
Redfox <nothing@nothing.org>
2009-04-07 18:27:16 +0200

    Fixed: Arcane Potency

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4828,6 +4828,19 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 triggered_spell_id = 29077;
                 break;
             }
+			//Arcane Potency
+			if (dummySpell->SpellIconID == 2120)
+			{
+				if(!procSpell)
+					return false;
+					
+				switch (dummySpell->Id)
+				{
+					case 31571: triggered_spell_id = 57529; break;
+					case 31572: triggered_spell_id = 57531; break;
+				}
+				break;
+			}
             // Hot Streak
             if (dummySpell->SpellIconID == 2999)
             {
@@ -10366,6 +10379,7 @@ bool InitTriggerAuraData()
     isTriggerAura[SPELL_AURA_PRAYER_OF_MENDING] = true;
     isTriggerAura[SPELL_AURA_PROC_TRIGGER_SPELL_WITH_VALUE] = true;
     isTriggerAura[SPELL_AURA_MOD_DAMAGE_FROM_CASTER] = true;
+	isTriggerAura[SPELL_AURA_MOD_SPELL_CRIT_CHANCE] = true;
 
     isNonTriggerAura[SPELL_AURA_MOD_POWER_REGEN]=true;
     isNonTriggerAura[SPELL_AURA_REDUCE_PUSHBACK]=true;
@@ -10625,6 +10639,10 @@ void Unit::ProcDamageAndSpellFor( bool isVictim, Unit * pTarget, uint32 procFlag
                 if (triggeredByAura->GetCasterGUID() != pTarget->GetGUID())
                     continue;
                 break;
+			case SPELL_AURA_MOD_SPELL_CRIT_CHANCE:
+				if (!procSpell)
+					continue;
+				break;
             default:
                 // nothing do, just charges counter
                 break;
